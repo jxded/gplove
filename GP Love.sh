b9398144bab311eca714516af69662553183d586
zaki <87823695+jxded@users.noreply.github.com>
2024-10-14 11:19:03 +0200

    Add files via upload

---
+++ GP Love.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+
+. /opt/muos/script/var/func.sh
+
+
+echo app >/tmp/act_go
+
+# Define paths and commands
+LOVEDIR="$(GET_VAR "device" "storage/rom/mount")/MUOS/application/.gplove"
+GPTOKEYB="$(GET_VAR "device" "storage/rom/mount")/MUOS/emulator/gptokeyb/gptokeyb2.armhf"
+CONFDIR="$LOVEDIR/conf/"
+
+# Export environment variables
+export SDL_GAMECONTROLLERCONFIG_FILE="/usr/lib/gamecontrollerdb.txt"
+export XDG_DATA_HOME="$CONFDIR"
+
+# Launcher
+cd "$LOVEDIR" || exit
+SET_VAR "system" "foreground_process" "love"
+export LD_LIBRARY_PATH="$LOVEDIR/libs:$LD_LIBRARY_PATH"
+$GPTOKEYB "love" &
+./love gp.love
+kill -9 "$(pidof gptokeyb2.armhf)"
+
