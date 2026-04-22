4d8eee70bc4d503f1069fbafa36f7868746fd73b
zaki <ahmed@zakii.de>
2026-04-22 21:28:04 +0200

    feat/cross compile, deploy

--- cmake/aarch64-toolchain-mac.cmake
+++ cmake/aarch64-toolchain-mac.cmake
@@ -6,6 +6,15 @@ set(CMAKE_C_COMPILER   aarch64-unknown-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER aarch64-unknown-linux-gnu-g++)
 set(CMAKE_STRIP        aarch64-unknown-linux-gnu-strip)
 
+if(NOT DEFINED TARGET_SYSROOT)
+  set(TARGET_SYSROOT "" CACHE PATH "Path to target sysroot")
+endif()
+
+if(TARGET_SYSROOT)
+  set(CMAKE_SYSROOT ${TARGET_SYSROOT})
+  set(CMAKE_FIND_ROOT_PATH ${TARGET_SYSROOT})
+endif()
+
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
